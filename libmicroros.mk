5dad554ed673173fac20d965d10d72d2a7e7e6ed
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-08-08 16:23:56 +0100

    Add tf2_msgs from geometry2 to libmicroros.mk
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- libmicroros.mk
+++ libmicroros.mk
@@ -80,11 +80,26 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	git clone -b humble https://github.com/ros2/rcl_logging src/rcl_logging; \
 	git clone -b humble https://gitlab.com/ros-tracing/ros2_tracing src/ros2_tracing; \
 	git clone -b humble https://github.com/micro-ROS/micro_ros_utilities src/micro_ros_utilities; \
+	git clone -b humble https://github.com/ros2/geometry2 src/geometry2; \
 	touch src/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE; \
 	touch src/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE; \
 	touch src/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE; \
 	touch src/rclc/rclc_examples/COLCON_IGNORE; \
 	touch src/rcl/rcl_yaml_param_parser/COLCON_IGNORE; \
+	touch src/geometry2/examples_tf2_py/COLCON_IGNORE; \
+	touch src/geometry2/geometry2/COLCON_IGNORE; \
+	touch src/geometry2/test_tf2/COLCON_IGNORE; \
+	touch src/geometry2/tf2/COLCON_IGNORE; \
+	touch src/geometry2/tf2_bullet/COLCON_IGNORE; \
+	touch src/geometry2/tf2_eigen/COLCON_IGNORE; \
+	touch src/geometry2/tf2_eigen_kdl/COLCON_IGNORE; \
+	touch src/geometry2/tf2_geometry_msgs/COLCON_IGNORE; \
+	touch src/geometry2/tf2_kdl/COLCON_IGNORE; \
+	touch src/geometry2/tf2_py/COLCON_IGNORE; \
+	touch src/geometry2/tf2_ros/COLCON_IGNORE; \
+	touch src/geometry2/tf2_ros_py/COLCON_IGNORE; \
+	touch src/geometry2/tf2_sensor_msgs/COLCON_IGNORE; \
+	touch src/geometry2/tf2_tools/COLCON_IGNORE; \
 	cp -rf $(EXTRA_ROS_PACKAGES) src/extra_packages || :; \
 	test -f src/extra_packages/extra_packages.repos && cd src/extra_packages && vcs import --input extra_packages.repos || :;
 
